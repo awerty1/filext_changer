a0d368ade5b6297b9bc715c2d3e1f1017168ffa4
pheno <testuniversity@yandex.ru>
2023-05-11 22:26:49 +0300

    1.added some comments, 2.fixed the logic of the count variable, 3.added singular and plural in file count message, 4.commented out the unnecessary part of the code

--- filext_changer.py
+++ filext_changer.py
@@ -2,8 +2,11 @@ import os
 from datetime import datetime
 
 '''
-Function to validate the input of the correct file extension 
-Accepts extensions '.!ut' & '.part'
+Function to validate the input of the correct file extension. 
+Accepts extensions '.!ut' || '.part'.
+
+#variables:
+*extension - file extension (.!ut || .part)
 '''
 
 
@@ -19,17 +22,21 @@ def get_valid_extension():
 
 
 '''
-A function that renames the extension ".! ut" to ".part" or vice versa
-directory - сurrent directory
-ext - file extension
+A function that renames the extension ".! ut" to ".part" or vice versa.
+
+#variables:
+*directory - current directory
+*extension - file extension
+*failed_file_path - 
 '''
 
 
 def rename_files(directory, extension, failed_file_path):
     try:
         counter = 0
-        counter_minus = 0
+        #counter_minus = 0
         failed_rename_files = []
+        print("")
         for filename in os.listdir(directory):
             if not filename.endswith(extension):
                 continue
@@ -41,37 +48,51 @@ def rename_files(directory, extension, failed_file_path):
 
             if os.path.exists(new_filename):
                 print(f"{counter + 1} \"{new_filename}\" - already exists, skipping")
-                counter += 1
-                counter_minus += 1
+                #counter += 1
+                #counter_minus += 1
                 failed_rename_files.append(filename)
             else:
                 os.rename(old_filename, new_filename)
                 print(f"{counter + 1} \"{filename}\" was renamed to \"{new_filename}\"")
-                counter += 1
 
-        counter -= counter_minus
-        print(f"File extension replacement completed successfully! {counter} files were renamed.")
+            counter += 1
+
+        #counter -= counter_minus
+        print(f"\nFile extension replacement completed successfully! {counter} files were renamed.")
 
         # Write filed files to a file
         failed_renames_files(failed_rename_files)
-        print(f"{len(failed_rename_files)} files failed to rename, check {failed_file_path}")
+        if len(failed_rename_files) == 1:
+            print(f"{len(failed_rename_files)} file failed to rename, check {failed_file_path}")
+        else:
+            print(f"{len(failed_rename_files)} files failed to rename, check {failed_file_path}")
     except FileNotFoundError:
         print(f"Directory \"{directory}\" not found.")
     except OSError:
         print(f"Cannot access directory \"{directory}\".")
 
 
-def failed_renames_files(failed_rename_files):
+'''
+A function to add information to a file about files that cannot be renamed because they already exist.
+The current date and message are also output to the file.
 
+#variables:
+*failed_rename_files - list of failed rename files
+*date_string - current date
+*failed_file_path - path to file
+'''
+
+
+def failed_renames_files(failed_rename_files):
+    # create a new file (Failed rename files(count).txt) if such a file is already contained in the directory
     failed_file_path = os.path.abspath(os.path.join(directory, "Failed rename files.txt"))
     count = 1
     while os.path.exists(failed_file_path):
         failed_file_path = os.path.abspath(os.path.join(directory, f"Failed rename files({count}).txt"))
-        #failed_file_path = f"{directory}failed rename files({count}).txt"
+        # failed_file_path = f"{directory}failed rename files({count}).txt"
         count += 1
 
-
-    #print(f"{failed_file_path}")
+    # print(f"{failed_file_path}")
 
     now = datetime.now()
     # Format date as string
@@ -107,6 +128,6 @@ if __name__ == '__main__':
     # extension = '.!ut'
     # extension = '.part'
     # failed_file_path = input("Enter path to save failed file names: ")
-    #failed_file_path = 'h:/CODE/Python/vogu project/File Extension/Failed rename files.txt'
+    # failed_file_path = 'h:/CODE/Python/vogu project/File Extension/Failed rename files.txt'
     failed_file_path = os.path.abspath(os.path.join(directory, 'Failed rename files.txt'))
     rename_files(directory, extension.lower(), failed_file_path)
