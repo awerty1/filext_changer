8d772e9b742e48012f04064436c1c93a1ec04f82
pheno <testuniversity@yandex.ru>
2023-05-21 18:22:55 +0300

    1. added comment to "remove_file_extension" 2. translated comments to English 3. added comment to "create_remove_ext_frm_files_log"

--- remove_filext.py
+++ remove_filext.py
@@ -5,6 +5,15 @@ from datetime import datetime
 from colorama import init, Fore, Style
 import choose_action
 
+'''
+Function to remove file extension
+
+#variables:
+*directory - the directory to search for files
+*extension_to_remove - the file extension to delete (e.g., ".txt")
+*remove_filext_path - file path for removed extension files
+'''
+
 
 def remove_file_extension(directory, extension_to_remove, remove_filext_path):
     start_time = time.time()
@@ -12,23 +21,23 @@ def remove_file_extension(directory, extension_to_remove, remove_filext_path):
     success_remove_ext_frm_files = {}
     failed_remove_ext_frm_files = {}
     try:
-        print("")
-        print(f"The extension change occurs in the directory "
+        # print("")
+        print(f"\nRemove files extensions occurs in the directory "
               f"{Fore.CYAN}{directory}{Fore.RESET}")
 
-        # перебираем все файлы в директории
+        # Iterate over all files in a directory
         for filename in os.listdir(directory):
-            # проверяем, что файл имеет нужное расширение
+            # Check that the file has the correct extension
             if filename.endswith(extension_to_remove):
                 filepath = os.path.join(directory, filename)
                 file_size = os.path.getsize(filepath)
                 try:
-                    # создаем новое имя файла без расширения
+                    # Create a new filename without extension
                     new_filename = os.path.splitext(filename)[0]
-                    # создаем новый путь к файлу без расширения
+                    # Create a new file path without extension
                     new_path = os.path.join(directory, new_filename)
                     new_path_basename = os.path.basename(new_path)
-                    # переименовываем файл
+                    # Rename the file
                     os.rename(os.path.join(directory, filename), new_path)
 
                     success_remove_ext_frm_files[filename] = file_size
@@ -48,10 +57,10 @@ def remove_file_extension(directory, extension_to_remove, remove_filext_path):
         formatted_time = choose_action.format_elapsed_time(elapsed_time)
 
         deleted_files_count = len(success_remove_ext_frm_files)
-        msg1_for_1 = f"\nDeleting files completed successfully! " \
+        msg1_for_1 = f"\nRemoved file extension completed successfully! " \
                      f"{deleted_files_count} file were deleted."
-        msg2_for_any = f"\nDeleting files completed successfully! " \
-                       f"{deleted_files_count} files were deleted."
+        msg2_for_any = f"\nRemoved files extensions completed successfully! " \
+                       f"{deleted_files_count} files extension were removed."
 
         if deleted_files_count == 1:
             print(msg1_for_1)
@@ -64,10 +73,10 @@ def remove_file_extension(directory, extension_to_remove, remove_filext_path):
 
         failed_deleted_files_count = len(failed_remove_ext_frm_files)
         msg1_for_1 = f"{failed_deleted_files_count} " \
-                     f"file failed to delete, check " \
+                     f"file failed to remove extension, check " \
                      f"{Fore.CYAN}{remove_filext_path}{Fore.RESET}"
         msg2_for_any = f"{failed_deleted_files_count} " \
-                       f"files failed to delete, check " \
+                       f"files failed to remove extension, check " \
                        f"{Fore.CYAN}{remove_filext_path}{Fore.RESET}"
 
         if failed_deleted_files_count == 1:
@@ -78,8 +87,19 @@ def remove_file_extension(directory, extension_to_remove, remove_filext_path):
         print(f"Directory {Fore.CYAN}{directory}{Fore.RESET} "
               f"does not exist")
     except Exception as e:
-        print(f"An error occurred while deleting files: "
+        print(f"An error occurred while removed files extension: "
               f"{Fore.LIGHTRED_EX}{str(e)}{Fore.RESET}")
+
+
+'''
+Function for logging
+
+#variables:
+*failed_remove_ext_frm_files - file path to unsuccessfully remove files extension
+*success_remove_ext_frm_files - dictionary of successfully remove files extension
+*remove_filext_path - file path to remove file extension
+elapsed_time - total file extension removal time
+'''
 
 
 def create_remove_ext_frm_files_log(failed_remove_ext_frm_files, success_remove_ext_frm_files,
@@ -105,19 +125,19 @@ def create_remove_ext_frm_files_log(failed_remove_ext_frm_files, success_remove_
         # write current directory
         f.write("Current directory: " + os.path.dirname(remove_filext_path) + "\n")
         # count of all deleted files
-        f.write(f"Total deleted files: {len(success_remove_ext_frm_files)}\n")
+        f.write(f"Total removed extension files: {len(success_remove_ext_frm_files)}\n")
         # count of failed deleted files
-        f.write(f"Total failed deleted files: {len(failed_remove_ext_frm_files)}\n")
+        f.write(f"Total failed removed extension files: {len(failed_remove_ext_frm_files)}\n")
         # total size of deleted files
-        f.write(f"Total size of deleted files: {total_size}\n")
+        f.write(f"Total size of removed extension files: {total_size}\n")
         # elapsed time
         f.write(f"Elapsed time: {elapsed_time} ms.\n\n")
 
         # write list of failed deleted files
         if failed_remove_ext_frm_files:
-            f.write('\n###########################')
-            f.write(f"\n# Unsuccessful deleted {len(failed_remove_ext_frm_files)}: #")
-            f.write('\n###########################\n\n')
+            f.write('\n#####################################')
+            f.write(f"\n# Unsuccessful removed extension {len(failed_remove_ext_frm_files)}: #")
+            f.write('\n#####################################\n\n')
             # add a number to each line of the list of failed deleted
             for i, (file, file_size) in enumerate(failed_remove_ext_frm_files.items(), start=1):
                 file_label = choose_action.format_size(file_size)
@@ -125,7 +145,7 @@ def create_remove_ext_frm_files_log(failed_remove_ext_frm_files, success_remove_
         # write msg is all files deleted successfully
         elif success_remove_ext_frm_files and not failed_remove_ext_frm_files:
             f.write('\n##############################################')
-            f.write('\n# Great job! All files deleted successfully! #')
+            f.write('\n# Great job! All files extension removed successfully! #')
             f.write('\n##############################################')
         # write msg if extension does not found in directory
         else:
@@ -135,9 +155,9 @@ def create_remove_ext_frm_files_log(failed_remove_ext_frm_files, success_remove_
 
         # write list of successfully deleted files
         if success_remove_ext_frm_files:
-            f.write('\n\n#########################')
-            f.write(f"\n# Successful deleted {len(success_remove_ext_frm_files)}: #")
-            f.write('\n#########################\n\n')
+            f.write('\n\n###################################')
+            f.write(f"\n# Successful removed extension {len(success_remove_ext_frm_files)}: #")
+            f.write('\n###################################\n\n')
             # add a number to each line of the list of successful deleted
             for i, (file, file_size) in enumerate(success_remove_ext_frm_files.items(), start=1):
                 file_label = choose_action.format_size(file_size)
