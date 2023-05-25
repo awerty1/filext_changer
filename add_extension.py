61c9e12d7562796e18a067df7770bd4ee5d8510c
pheno <testuniversity@yandex.ru>
2023-05-25 21:08:29 +0300

    1. added some comments 2. rename variables

--- add_extension.py
+++ add_extension.py
@@ -6,10 +6,16 @@ from colorama import init, Fore, Style
 import choose_action
 
 '''
+Function to adding an extension to files without an extension
+
+#variables:
+*directory - the directory to search for files
+*add_extension_to_file - the file extension to add (e.g., ".txt")
+*remove_filext_path - file path for removed extension files
 '''
 
 
-def add_extension(directory: str, add_extension_to_file: str, remove_filext_path: str) -> None:
+def add_extension(directory: str, add_extension_to_file: str, add_filext_path: str) -> None:
     start_time = time.time()
     file_counter = 1
     success_add_ext_to_files = {}
@@ -36,7 +42,7 @@ def add_extension(directory: str, add_extension_to_file: str, remove_filext_path
                     print(f"{file_counter}. "
                           f"{Fore.GREEN}To file{Fore.RESET} "
                           f"{Fore.WHITE}{filename}{Fore.RESET}"
-                          f" {Fore.GREEN}was successfully added extension {Fore.RESET} "
+                          f" {Fore.GREEN}was successfully added extension{Fore.RESET} "
                           f"{Fore.BLUE + Style.BRIGHT}{new_path_basename}{Style.RESET_ALL}")
                 except FileExistsError:
                     failed_add_ext_to_files[filename] = file_size
@@ -64,16 +70,16 @@ def add_extension(directory: str, add_extension_to_file: str, remove_filext_path
             print(msg2_for_any)
 
         # Write status of deleted files to a file
-        remove_filext_path = create_add_ext_files_log(failed_add_ext_to_files, success_add_ext_to_files,
-                                                      remove_filext_path, formatted_time)
+        add_filext_path = create_add_ext_files_log(failed_add_ext_to_files, success_add_ext_to_files,
+                                                   add_filext_path, formatted_time)
 
         failed_deleted_files_count = len(failed_add_ext_to_files)
         msg1_for_1 = f"{Fore.BLUE}{failed_deleted_files_count}{Fore.RESET} " \
                      f"{Fore.RED}file failed to add extension, check{Fore.RESET} " \
-                     f"{Fore.BLUE}{remove_filext_path}{Fore.RESET}"
+                     f"{Fore.BLUE}{add_filext_path}{Fore.RESET}"
         msg2_for_any = f"{Fore.BLUE}{failed_deleted_files_count}{Fore.RESET} " \
                        f"{Fore.RED}files failed to add extension, check{Fore.RESET} " \
-                       f"{Fore.BLUE}{remove_filext_path}{Fore.RESET}"
+                       f"{Fore.BLUE}{add_filext_path}{Fore.RESET}"
 
         if failed_deleted_files_count == 1:
             print(msg1_for_1)
@@ -89,10 +95,17 @@ def add_extension(directory: str, add_extension_to_file: str, remove_filext_path
 
 
 '''
+Function for saving information about operations to a file
+
+#variables:
+*failed_remove_ext_frm_files 
+*success_remove_ext_frm_files
+*remove_filext_path
+*elapsed_time
 '''
 
 
-def create_add_ext_files_log(failed_remove_ext_frm_files: dict[str, int],
+def create_add_ext_files_log(failed_added_ext_to_files: dict[str, int],
                              success_remove_ext_frm_files: dict[str, int],
                              remove_filext_path: str, elapsed_time: str) -> str:
     # create a new file (failed deleted files(count).txt)
@@ -119,23 +132,23 @@ def create_add_ext_files_log(failed_remove_ext_frm_files: dict[str, int],
         # count of all deleted files
         f.write(f"Total removed extension files: {len(success_remove_ext_frm_files)}\n")
         # count of failed deleted files
-        f.write(f"Total failed removed extension files: {len(failed_remove_ext_frm_files)}\n")
+        f.write(f"Total failed removed extension files: {len(failed_added_ext_to_files)}\n")
         # total size of deleted files
         f.write(f"Total size of removed extension files: {total_size}\n")
         # elapsed time
         f.write(f"Elapsed time: {elapsed_time} ms.\n\n")
 
-        # write list of failed deleted files
-        if failed_remove_ext_frm_files:
+        # write list of failed extension added to file
+        if failed_added_ext_to_files:
             f.write('\n############################################')
-            f.write(f"\n# Unsuccessful added extension {len(failed_remove_ext_frm_files)}: #")
+            f.write(f"\n# Unsuccessful added extension {len(failed_added_ext_to_files)}: #")
             f.write('\n############################################\n\n')
-            # add a number to each line of the list of failed deleted
-            for i, (file, file_size) in enumerate(failed_remove_ext_frm_files.items(), start=1):
+            # add a number to each line of the list of failed added extension
+            for i, (file, file_size) in enumerate(failed_added_ext_to_files.items(), start=1):
                 file_label = choose_action.format_size(file_size)
                 f.write(f"{i}. {file}  {file_label}\n")
-        # write msg is all files deleted successfully
-        elif success_remove_ext_frm_files and not failed_remove_ext_frm_files:
+        # write msg is all file extensions added successfully
+        elif success_remove_ext_frm_files and not failed_added_ext_to_files:
             f.write('\n#####################################################')
             f.write('\n# Great job! All file extension added successfully! #')
             f.write('\n#####################################################')
@@ -145,12 +158,12 @@ def create_add_ext_files_log(failed_remove_ext_frm_files: dict[str, int],
             f.write('\n# Files not found, no such extension! #')
             f.write('\n#######################################')
 
-        # write list of successfully deleted files
+        # write list of successfully added files extensions
         if success_remove_ext_frm_files:
             f.write('\n\n################################################')
             f.write(f"\n# Successful added extension to empty files {len(success_remove_ext_frm_files)}: #")
             f.write('\n################################################\n\n')
-            # add a number to each line of the list of successful deleted
+            # add a number to each line of the list of successful added extensions
             for i, (file, file_size) in enumerate(success_remove_ext_frm_files.items(), start=1):
                 file_label = choose_action.format_size(file_size)
                 f.write(f"{i}. {file}  {file_label}\n")
