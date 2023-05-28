eef47c25e28b9802af3f63b1236597b5a68ecd40
pheno <testuniversity@yandex.ru>
2023-05-28 23:26:31 +0300

    1. added main_config 4 config 2. added comment and configs frm main_config 3. added variables to main_conf

---
+++ main_config.py
@@ -0,0 +1,22 @@
+"""
+config 4 main_conf
+"""
+# directory of the folder
+directory = 'h:/CODE/Python/vogu project/File Extension/fd/'
+
+# extension of file
+extension_ut = '.!ut'
+extension_part = '.part'
+
+# 1 - rename , 2 - delete , 3 - remove, 4 - added ext to empty file , 5 - added ext to ext
+action = 2
+
+
+'''
+config 4 main
+'''
+name_of_renamed_files = 'Renamed files.txt'
+name_of_deleted_files = 'Deleted files.txt'
+name_of_remove_extensions_file = 'Removed extension.txt'
+name_of_added_ext_file = 'Added extension.txt'
+name_of_added_ext_to_ext_file = 'Added extension to extension.txt'
